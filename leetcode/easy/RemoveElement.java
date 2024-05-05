71b15d021cd3066d5f35efe12500a4f041c7c607
Manlin Guo <manlin1119@gmail.com>
2024-05-05 00:13:26 -0400

    Update

---
+++ leetcode/easy/RemoveElement.java
@@ -0,0 +1,25 @@
+/* 
+27. Remove Element
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Note: Two pointers O(n) 具有单调性
+
+*/
+
+class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
+
+
