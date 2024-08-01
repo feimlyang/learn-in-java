5578bd828b422d824d271fe3586c644b7ae32e0d
Manlin Guo <manlin1119@gmail.com>
2024-08-01 19:43:32 -0400

    Update

---
+++ leetcode/medium/PopulatingNextRightPointersinEachNode2.java
@@ -0,0 +1,41 @@
+/*
+117. Populating Next Right Pointers in Each Node II
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+
+
+class Solution {
+    public Node connect(Node root) {
+        List<List<Node>> res = new ArrayList<>();
+        preorder(root, 0, res);
+        return root;       
+    }
+
+    private void preorder(Node node, int level, List<List<Node>> res) {
+        if (node == null) return;
+        if (res.size() - 1 < level) {
+            List<Node> list = new ArrayList<>();
+            list.add(node);
+            res.add(list);
+        } else {
+            List<Node> list = res.get(level);
+            Node last = list.get(list.size() - 1);
+            last.next = node;
+            node.next = null;
+            list.add(node);            
+        }
+        level++;
+        preorder(node.left, level, res);
+        preorder(node.right, level, res);
+    }
+}
