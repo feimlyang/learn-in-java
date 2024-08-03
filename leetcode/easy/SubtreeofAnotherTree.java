28e49f92691c487b66899f73598d9e73828631ad
Manlin Guo <manlin1119@gmail.com>
2024-08-03 02:10:53 -0400

    Update

---
+++ leetcode/easy/SubtreeofAnotherTree.java
@@ -0,0 +1,39 @@
+/*
+572. Subtree of Another Tree
+
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) return true;
+        else if (root == null) return false;
+        return compare(root, subRoot) || 
+            isSubtree(root.left, subRoot) || 
+            isSubtree(root.right, subRoot); 
+    }
+
+    public boolean compare(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        else if (p == null || q == null) return false;
+        else if (p.val != q.val) return false;
+        return compare(p.left, q.left) && compare(p.right, q.right);
+    }
+}
